902b5f4fb177df69f32447db48ce9dad76cd5ee2
Jake Goritski <jgoritski@gmail.com>
2022-12-27 22:56:03 -0500

    init views

--- bkeys/urls.py
+++ bkeys/urls.py
@@ -18,7 +18,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from builder.views import HomeView
+
 urlpatterns = [
+    path("", HomeView.as_view()),
     path("admin/", admin.site.urls),
 ]
 
